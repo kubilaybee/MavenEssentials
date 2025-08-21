63425610e4c66f42bd21cb3d9f05cfdf5e92dc05
Kubilay <kubilaybee@gmail.com>
2025-08-21 18:03:24 +0300

    feat: initialize maven essentials

---
+++ maven-essentials/src/main/java/com/springprojects/maven_essentials/MavenEssentialsApplication.java
@@ -0,0 +1,14 @@
+package com.springprojects.maven_essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenEssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenEssentialsApplication.class, args);
+		System.out.println("maven essentials");
+	}
+
+}
---
+++ maven-essentials/src/test/java/com/springprojects/maven_essentials/MavenEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springprojects.maven_essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
